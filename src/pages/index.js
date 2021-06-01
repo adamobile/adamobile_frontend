ccfe9cddec3b0886bbe64180ce68ec3f6ed3efd1
Alexy Georgos <alexygeorgos@hotmail.com>
2021-06-01 19:49:22 +0200

    add websocket test

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,24 @@ const API = "http://localhost:8001"
 
 const IndexPage = (props) => {
 
-
-  const [stats, setStats] = React.useState({"total":0,"minted":0,"minting":0,"available":0});
+  const [stats, setStats] = React.useState({'total':0,'minted':0,'minting':0,'available':0});
 
   const fetchStats = async () => {
     await fetch(API + "/stats").then((res) => res.json()).then((data) => setStats(data));
   };
+
+  var ws = new WebSocket('ws://localhost:8080', "protocolOne");
+  ws.onopen = function (event) {
+    console.log('connected to web socket server');
+    ws.send('Here\'s some text that the server is urgently awaiting!');
+  };
+  ws.onmessage = function (event) {
+    console.log(event.data);
+  }
+
+  const sendMsg = () => {
+    ws.send('Here\'s some text that the server is urgently awaiting!');
+  }
 
   /* React.useEffect(() => {
     const interval = setInterval(() => {
@@ -24,7 +36,8 @@ const IndexPage = (props) => {
     <Layout pageTitle="Welcome to ADAmobile">
     <p>Grab your ADAmobile and join the ride!</p>
     <Stats stats={stats}></Stats><br/>
-    <button onClick={()=>{fetchStats();}}>Refresh Statistics</button>
+    <button onClick={()=>{fetchStats();}}>Refresh Statistics</button><br/>
+    <button onClick={()=>{sendMsg();}}>Send Message</button>
     </Layout>
   )
 }
