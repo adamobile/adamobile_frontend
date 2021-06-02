d3d826d98b76cead7a800bb8ea416a1e26718a53
Ada <Mobile>
2021-06-02 16:36:06 +0200

    get stats via web socket

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,30 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Stats from '../components/stats'
+const WebSocket = require('isomorphic-ws');
 
 const API = "http://localhost:8001"
+
+var ws = new WebSocket('ws://localhost:8080');
+ws.onopen = function (event) {
+  console.log('Connection is open!');
+};
+
+
+
+const sendMsg = () => {
+  ws.send('Here\'s some text that the server is urgently awaiting!');
+}
 
 const IndexPage = (props) => {
 
   const [stats, setStats] = React.useState({'total':0,'minted':0,'minting':0,'available':0});
 
-  const fetchStats = async () => {
-    await fetch(API + "/stats").then((res) => res.json()).then((data) => setStats(data));
-  };
-
-  var ws = new WebSocket('ws://localhost:8080', "protocolOne");
-  ws.onopen = function (event) {
-    console.log('connected to web socket server');
-    ws.send('Here\'s some text that the server is urgently awaiting!');
-  };
   ws.onmessage = function (event) {
-    console.log(event.data);
-  }
-
-  const sendMsg = () => {
-    ws.send('Here\'s some text that the server is urgently awaiting!');
-  }
+    console.log('received: ', event.data);
+    const msg = JSON.parse(event.data);
+    setStats(msg.stats)
+  };
 
   /* React.useEffect(() => {
     const interval = setInterval(() => {
@@ -36,8 +37,6 @@ const IndexPage = (props) => {
     <Layout pageTitle="Welcome to ADAmobile">
     <p>Grab your ADAmobile and join the ride!</p>
     <Stats stats={stats}></Stats><br/>
-    <button onClick={()=>{fetchStats();}}>Refresh Statistics</button><br/>
-    <button onClick={()=>{sendMsg();}}>Send Message</button>
     </Layout>
   )
 }
