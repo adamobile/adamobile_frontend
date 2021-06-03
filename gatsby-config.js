7cffbb88015bcf8ddfea35ba1d582863ee3a6766
Alexy Georgos <alexygeorgos@hotmail.com>
2021-06-03 22:56:16 +0200

    reset to stay tuned

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: "AdaMobile",
   },
-  plugins: ["gatsby-plugin-emotion"],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,62 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import Stats from '../components/stats'
-const WebSocket = require('isomorphic-ws');
+import { StaticImage } from "gatsby-plugin-image"
+import styled from "@emotion/styled"
+import "../css/main.css";
 
-const API = "http://localhost:8001"
+const Wrapper = styled("div")`
+  margin: auto;
+`
 
-var ws = new WebSocket('ws://localhost:8080');
-ws.onopen = function (event) {
-  console.log('Connection is open!');
-};
+const Banner = styled("div")`
 
+`
 
+const Logo = styled("div")`
+  position: absolute;
+  top: 10px;
+  left: 16px;
+  width: 400px;
+  height: 400px;
+`
 
-const sendMsg = () => {
-  ws.send('Here\'s some text that the server is urgently awaiting!');
-}
+const InfoBox = styled("div")`
+  position: absolute;
+  top: 350px;
+  left: 16px;
+  color: white;
+  font-size: 1.5em;
+  font-family: lato, sans-serif;
+`
+const StayTuned = styled("div")`
+  font-family: dodger;
+  font-size: 3.5em;
+  color: rgb(176, 34, 38);
+  text-align: center;
+`
 
 const IndexPage = (props) => {
 
-  const [stats, setStats] = React.useState({'total':0,'minted':0,'minting':0,'available':0});
-
-  ws.onmessage = function (event) {
-    console.log('received: ', event.data);
-    const msg = JSON.parse(event.data);
-    setStats(msg.stats)
-  };
-
-  /* React.useEffect(() => {
-    const interval = setInterval(() => {
-      fetchStats();
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []); */
-
   return (
-    <Layout pageTitle="Welcome to ADAmobile">
-    <p>Grab your ADAmobile and join the ride!</p>
-    <Stats stats={stats}></Stats><br/>
-    </Layout>
+    <Wrapper>
+      <Banner>
+        <StaticImage
+          src="../images/stay_tuned.png"
+          alt="Stay tuned"/>
+      </Banner>
+      <Logo>
+        <StaticImage
+          src="../images/logo.png"
+          alt="Adamobile logo"/>
+      </Logo>
+      <InfoBox>
+        <p>Adamobiles are NFT-Collectables on the cardano blockchain</p>
+        <p>You can buy, sell, or gift Adamobiles. Or simply enjoy them parked in your wallet</p>
+      </InfoBox>
+      <StayTuned>
+        <p>Engines starting soon</p>
+        <p>Stay tuned!</p>
+      </StayTuned>
+    </Wrapper>
   )
 }
 
